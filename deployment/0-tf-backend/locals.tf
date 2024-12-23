53ce663a685c62a52722d5ad164669d35cdb904e
Gregor Schäfer <g.schaefer1990+github@gmail.com>
2024-12-24 00:16:27 +0100

    refactor

--- deployment/0-tf-backend/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  project = "appchips"
-}
--- deployment/0-tf-backend/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "terraform_state_bucket" {
-  value = aws_s3_bucket.terraform_state_bucket.bucket
-}
-
-output "bucket_suffix" {
-  value = random_string.bucket_suffix.result
-}
